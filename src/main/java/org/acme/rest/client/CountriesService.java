0951781d830fa3da2934350edfc5f6649b99a82f
Oscar <3311764+oscarfh@users.noreply.github.com>
2020-12-11 17:29:24 -0300

    Initial commit

---
+++ src/main/java/org/acme/rest/client/CountriesService.java
@@ -0,0 +1,18 @@
+package org.acme.rest.client;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.Set;
+
+@Path("/v2")
+@RegisterRestClient
+public interface CountriesService {
+
+	@GET
+	@Path("/name/{name}")
+	Set<Country> getByName(@PathParam String name);
+}
